e12d931f087391ee42535bcaa63751c255a98259
DESKTOP-BH9LEQR\domot <domoto0315@gmail.com>
2022-07-31 01:04:34 +0900

    スペルミス修正、不要なインポートの削除

--- src/main/java/com/shoppingsite/auth/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/shoppingsite/auth/CustomAuthenticationFailureHandler.java
@@ -1,20 +1,14 @@
 package com.shoppingsite.auth;
 
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class CustomAuthenticationFailureHandler
 implements AuthenticationFailureHandler {
@@ -25,9 +19,9 @@ implements AuthenticationFailureHandler {
 			HttpServletResponse response,
 			AuthenticationException exception)
 					throws IOException, ServletException {
-		String fowardUrl = "/login?error";
+		String forwardUrl = "/login?error";
 
-		RequestDispatcher dispatch = request.getRequestDispatcher(fowardUrl);
+		RequestDispatcher dispatch = request.getRequestDispatcher(forwardUrl);
 		// フォワードを使うことでメールアドレスの入力値を保持
 		dispatch.forward(request, response);
 		
--- src/main/java/com/shoppingsite/controller/LoginController.java
+++ src/main/java/com/shoppingsite/controller/LoginController.java
@@ -1,20 +1,13 @@
 package com.shoppingsite.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.shoppingsite.auth.User;
